39db7d839f852126be39f7ffa795c8a63f21221a
Baryshnikov Nikolay <nabarysh@gmail.com>
2021-05-23 16:09:59 +0300

    stupid me

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -3,20 +3,42 @@ from django.shortcuts import render
 import csv
 
 
+def rgb2hex(r, g, b):
+    return "#{:02x}{:02x}{:02x}".format(r, g, b)
+
+
+def color_it(val: str):
+    color = rgb2hex(255, 255, 255)
+    try:
+        f = float(val)
+        if f < 0:
+            color = rgb2hex(0, 128, 0)
+        if f > 1:
+            red = 255 - (min([round(f), 6])) * 20
+            color = rgb2hex(red, 0, 0)
+    except Exception:
+        pass
+
+    return color
+
+
 def inflation_view(request):
-    template_name = 'inflation.html'
-    print(settings.INFL_CSV)
+    template_name = 'my_content.html'
     with open(settings.INFL_CSV, encoding='utf8') as csvf:
-        reader = csv.DictReader(csvf, delimiter=';')
-        DATA = [row for row in reader]
-        header_ = list(DATA[0].keys())
-        # тут надо обработку стринга !!!!!!!!!
-        body_ = [[int(list(row.values())[0])] + [float(item) for item in list(row.values())[1:]] for row in DATA[1:]]
-        print(body_)
+        reader = csv.reader(csvf, delimiter=';')
+        data = [row for row in reader]
 
+    body_ = []
+    for row in data:
+        dataline = [{'value': item, 'color': None} for item in row]
+        body_ += [dataline]
 
-    # чтение csv-файла и заполнение контекста
-    context = {}
+    for row in body_[1:]:
+        row[-1]['color'] = rgb2hex(169, 169, 169)
+        for item in row[1:-1]:
+            item['color'] = color_it(item['value'])
 
-    return render(request, template_name,
-                  context)
+    c = {'data': body_}
+    print(c)
+
+    return render(request, template_name, context=c)
