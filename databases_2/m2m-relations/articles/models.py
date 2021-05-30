14f592a015e53c9ec85ac056f496768c18681d91
Baryshnikov Nikolay <nabarysh@gmail.com>
2021-05-30 12:49:43 +0300

    stupid topics done

--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -15,24 +15,8 @@ class Article(models.Model):
         return self.title
 
 
-# class Scope(models.Model):
-#     topic = models.CharField(max_length=256, verbose_name='Тема')
-#     article = models.ManyToManyField(Article, through='Scope_Article', related_name='scopes')
-#
-#     def __str__(self):
-#         return self.topic
-#
-#
-# class Scope_Article(models.Model):
-#     article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='статья')
-#     scope = models.ForeignKey(Scope, on_delete=models.CASCADE, verbose_name='тема')
-#     is_main = models.BooleanField(verbose_name='Основная тема', default=False)
-#
-#     def __str__(self):
-#         return f'{self.scope} - {self.article} - {self.is_main}'
-
 class Topic(models.Model):
-    topic = models.CharField(max_length=256, verbose_name='Тема')
+    topic = models.CharField(max_length=256, unique=True, verbose_name='Тема')
     article = models.ManyToManyField(Article, through='Scopes', related_name='topics')
 
     def __str__(self):
@@ -43,6 +27,9 @@ class Scopes(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='статья', related_name='scopes')
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, verbose_name='тема')
     is_main = models.BooleanField(verbose_name='Основная тема', default=False)
+
+    class Meta:
+        unique_together = ('article', 'topic')
 
     def __str__(self):
         return f'{self.topic} - {self.article} - {self.is_main}'
--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -10,15 +10,4 @@ def articles_list(request):
     a = Article.objects.all().order_by(ordering).prefetch_related('scopes')
     context = {'object_list': a}
 
-    # a = Article.objects.first().scopes.first().is_main
-    # print(a)
-    # s = Scope.objects.first()
-    # iii = Scope_Article.objects.get(article=a, scope=s)
-    #
-    # print(iii)
-
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-
     return render(request, template, context)
