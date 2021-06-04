a6c159d3c30eae7a11a28ddda28251889661d935
mexalon <nabarysh@gmail.com>
2021-06-04 16:51:18 +0300

    drf1 looks done

--- drf-intro/simple_crud/simple_crud/settings.py
+++ drf-intro/simple_crud/simple_crud/settings.py
@@ -80,7 +80,9 @@ WSGI_APPLICATION = 'simple_crud.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_simple_crud',
+        'NAME': 'hw_drf_1',
+        'USER': 'hw_drf_1',
+        'PASSWORD': 'hw_drf_1',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
